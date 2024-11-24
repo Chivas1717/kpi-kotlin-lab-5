9394020c7ec2eb01382f4cf78c4aa890aec55a52
Mark Hudzovskyi <markgud1717@gmail.com>
2024-11-24 00:08:25 -0500

    lab 5 done

---
+++ app/src/main/java/com/example/kpikotlinlab5/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.example.kpikotlinlab5.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFCF4040)
+val PurpleGrey80 = Color(0xFFFAA4C2)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFFB61016)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
